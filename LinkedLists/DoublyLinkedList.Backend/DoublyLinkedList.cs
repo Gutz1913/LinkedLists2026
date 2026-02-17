7a5a0893002305dc748e292c14e5e2478d097bb9
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 13:59:14 -0500

    Class Doubly Linked List

--- LinkedLists/DoublyLinkedList.Backend/DoublyLinkedList.cs
+++ LinkedLists/DoublyLinkedList.Backend/DoublyLinkedList.cs
@@ -1,5 +1,105 @@
 ﻿namespace DoublyLinkedList.Backend;
 
-public class DoublyLinkedList
+public class DoublyLinkedList<T>
 {
+    private DoubleNode<T>? _head;
+    private DoubleNode<T>? _tail;
+
+    public DoublyLinkedList()
+    {
+        _tail = null;
+        _head = null;
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null)  //Empty list
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+        }
+    }
+
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_tail == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Prev = _tail;
+            _tail!.Next = newNode;
+            _tail = newNode;
+        }
+    }
+
+    //Prints the list from head to tail
+    public string GetForward()
+    {
+        var output = string.Empty;
+        var current = _head;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Next;
+        }
+        return output.Substring(0, output.Length - 5);
+    }
+
+    //Prints the list from tail to head
+    public string GetBackward()
+    {
+        var output = string.Empty;
+        var current = _tail;
+        while (current != null)
+        {
+            output += $"{current.Data} <=> ";
+            current = current.Prev;
+        }
+        return output.Substring(0, output.Length - 5);
+    }
+
+    public void Remove(T data)
+    {
+        if (_head == null)     //Empty list
+        {
+            return;
+        }
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                if (current.Prev != null)
+                {
+                    current.Prev.Next = current.Next;
+                }
+                else
+                {
+                    _head = current.Next; // Update head if needed
+                }
+
+                if (current.Next != null)
+                {
+                    current.Next.Prev = current.Prev;
+                }
+                else
+                {
+                    _tail = current.Prev; // Update tail if needed
+                }
+                return; // Data found and removed, exit the method
+            }
+            current = current.Next;
+        }
+    }
 }
