d8a13b69b8568ca64df0e5013e130de5f4da0c17
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 13:49:14 -0500

    Class Singly Linked List and Single Node

---
+++ LinkedLists/SinglyLinkedList.Backend/SingleNode.cs
@@ -0,0 +1,14 @@
+﻿namespace SinglyLinkedList.Backend;
+
+public class SingleNode<T>
+{
+    public SingleNode(T data)
+    {
+        Data = data;
+        Next = null;
+    }
+
+    public T Data { get; set; }
+
+    public SingleNode<T>? Next { get; set; }
+}
