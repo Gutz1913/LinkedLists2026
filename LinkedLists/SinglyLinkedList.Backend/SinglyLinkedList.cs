1acb6d329137011aa7e5606b0c9cf60cb6d905c3
Bacudo1991 <andresgutierrez1135155@correo.itm.edu.co>
2026-02-17 13:51:34 -0500

    Class Singly Linked List Backend

---
+++ LinkedLists/SinglyLinkedList.Backend/SinglyLinkedList.cs
@@ -0,0 +1,88 @@
+﻿namespace SinglyLinkedList.Backend;
+
+public class SinglyLinkedList<T>
+{
+    private SingleNode<T>? _head;
+
+    public SinglyLinkedList()
+    {
+        _head = null;
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new SingleNode<T>(data);
+        newNode.Next = _head;
+        _head = newNode;
+    }
+
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new SingleNode<T>(data);
+        if (_head == null)
+        {
+            _head = newNode;
+        }
+        else
+        {
+            var current = _head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public bool Contains(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                return true;
+            }
+            current = current.Next;
+        }
+        return false;
+    }
+
+    public void Remove(T data)
+    {
+        if (_head == null)
+        {
+            return;
+        }
+        if (_head.Data!.Equals(data))
+        {
+            _head = _head.Next;
+            return;
+        }
+        var current = _head;
+        while (current.Next != null && !current.Next.Data!.Equals(data))
+        {
+            current = current.Next;
+        }
+        if (current.Next != null)
+        {
+            current.Next = current.Next.Next;
+        }
+    }
+
+    public void Clear()
+    {
+        _head = null;
+    }
+
+    public void PrintList()
+    {
+        var current = _head;
+        while (current != null)
+        {
+            Console.Write($"{current.Data} -> ");
+            current = current.Next;
+        }
+        Console.WriteLine("null");
+    }
+}
